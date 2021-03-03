321306299b34ef779f0b3e2452da7a305d48ef9e
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-03-03 16:47:32 +0200

    added a backing bean

---
+++ src/main/java/com/tinyiko/jsf/CatalogItemForBean.java
@@ -0,0 +1,29 @@
+package com.tinyiko.jsf;
+
+public class CatalogItemForBean {
+	
+	private String name;
+	private String manufacture;
+	private String sku;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getManufacture() {
+		return manufacture;
+	}
+	public void setManufacture(String manufacture) {
+		this.manufacture = manufacture;
+	}
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	
+	
+}
