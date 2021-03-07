41b83c60d499aef2715b44bf0c856ee4f6fda66d
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-03-07 08:28:38 +0200

    10 added inventory class

---
+++ src/main/java/com/tinyiko/jsf/InvetoryItem.java
@@ -0,0 +1,59 @@
+package com.tinyiko.jsf;
+
+public class InvetoryItem {
+	
+	private Long inventoryItemId;
+	private Long catalogItemId;
+	private String name;
+	private Long quantity;
+	
+	
+	public InvetoryItem() {
+		
+	}
+
+	public InvetoryItem(Long inventoryItemId, Long catalogItemId, String name, Long quantity) {
+		super();
+		this.inventoryItemId = inventoryItemId;
+		this.catalogItemId = catalogItemId;
+		this.name = name;
+		this.quantity = quantity;
+	}
+
+	public Long getInventoryItemId() {
+		return inventoryItemId;
+	}
+
+	public void setInventoryItemId(Long inventoryItemId) {
+		this.inventoryItemId = inventoryItemId;
+	}
+
+	public Long getCatalogItemId() {
+		return catalogItemId;
+	}
+
+	public void setCatalogItemId(Long catalogItemId) {
+		this.catalogItemId = catalogItemId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Long quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	
+	
+
+}
