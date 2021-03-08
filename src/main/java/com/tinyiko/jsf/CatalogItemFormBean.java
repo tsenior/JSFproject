16ced3f5583aa3d0593bf43676b268ec9b6837e9
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-03-08 23:18:07 +0200

    add

--- src/main/java/com/tinyiko/jsf/CatalogItemFormBean.java
+++ src/main/java/com/tinyiko/jsf/CatalogItemFormBean.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ejb.EJB;
+
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
